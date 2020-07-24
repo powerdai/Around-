0fead1db2c29c0535e422fc53f98bb2f11230be3
powerdai <dwr0828@gmail.com>
2020-07-24 11:35:28 -0700

    Update main.go

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ const (
       //PROJECT_ID = "around-xxx"
       //BT_INSTANCE = "around-post"
       // Needs to update this URL if you deploy it to cloud.
-      ES_URL = "http://0.0.0.0:9200"
+      ES_URL = "http://34.72.220.63:9200"
 )
 
 func main() {
